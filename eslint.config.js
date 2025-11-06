6d399a95accc7883cab365f6879689aa3783c2ad
ivan <ivann97n@gmail.com>
2025-11-06 18:04:18 +0300

    feat: install dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,57 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier, // Должен быть последним
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_', // игнорировать переменные, начинающиеся с _
+          varsIgnorePattern: '^_', 
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn', // запрет использования any
+
+      // React
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      
+      // Общие правила
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+
+      // Импорты для FSD
+      'no-restricted-imports': [
+        'warn',
+        {
+          patterns: [
+            {
+              group: ['../*', '../../*', '../../../*'],
+              message: 'Используйте алиасы (@/shared, @/widgets) вместо относительных путей',
+            },
+          ],
+        },
+      ],
     },
   },
 ])
